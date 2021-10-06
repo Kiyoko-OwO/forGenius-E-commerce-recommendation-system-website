b15820432fe602d08b568e124259c42096791395
kevinChnn <cky1102115652@gmail.com>
2021-10-06 14:24:04 +0800

    Add sample HTTP response

--- forGenius/forGenius/urls.py
+++ forGenius/forGenius/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from user.views import register
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register),
 ]

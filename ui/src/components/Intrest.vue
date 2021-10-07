2d3aef3565090852533c53e7cf9fefac8ab54ba2
Minghao Cai <cai9981@outlook.com>
2021-10-07 19:49:55 +0800

    added interest field draft

---
+++ ui/src/components/Intrest.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Intrest Filed</h1>
+    <el-checkbox-group v-model="intest">
+      <el-checkbox-button v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox-button>
+    </el-checkbox-group>
+    <el-button class='submit'>SUBMIT</el-button>
+  </div>
+
+</template>
+<script>
+  const tagOptions = ['Fastion', 'Toys', 'Hobby', 'DIY','Electronics', 'Media', 'Furniture', 'Appliance','Food','Personal Care'];
+  export default {
+    data () {
+      return {
+        cities: tagOptions,
+        intest:[]
+      };
+    }
+  }
+</script>
+
+<style>
+.submit{
+    position: absolute;
+    left:50%;
+    bottom:12%;
+    height:50px;
+    width:200px;
+    transform: translate(-50%,-50%);
+    border-radius: 10px;
+    background: #786662;
+    color: #fefefe;
+    letter-spacing:10px;
+    padding-left: 30px;
+}
+</style>
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Signup from '../components/Signup.vue'
 import Home from '../components/Home.vue'
-
+import Intrest from '../components/Intrest.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -18,6 +18,10 @@ const routes = [
   {
     path: '/home',
     component: Home
+  },
+  {
+    path: '/intrest',
+    component: Intrest
   }
 ]
 

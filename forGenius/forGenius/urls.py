5d187b41002d5c0b21c4c28eff70a1d16c2df853
kevinChnn <cky1102115652@gmail.com>
2021-10-07 19:25:24 +0800

    Build first user register HTTP API

--- forGenius/forGenius/urls.py
+++ forGenius/forGenius/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from user.views import register
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register),
+    path('user/', include('user.urls', namespace='user')),
 ]
--- forGenius/user/views.py
+++ forGenius/user/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.datastructures import MultiValueDictKeyError
+import user.auth as auth
 
 # Create your views here.
+@csrf_exempt
 def register(request):
-    return HttpResponse("Hello world！")
+    response = HttpResponse()
+    if request.method == "POST":
+        try:
+            email = request.GET['email']
+            name = request.GET['name']
+            password = request.GET['password']
+        except MultiValueDictKeyError:
+            response.status_code = 400
+            return response
+        auth.auth_register(email, name, password)
+        response.status_code = 200
+        return response
+    response.status_code = 400
+    return response

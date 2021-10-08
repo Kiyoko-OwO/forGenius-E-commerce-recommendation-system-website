10b16807c25912b4f6dccc7d902a25a63eefe8d7
kevinChnn <cky1102115652@gmail.com>
2021-10-08 10:11:07 +0800

    Remove csrf check

--- forGenius/user/views.py
+++ forGenius/user/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.utils.datastructures import MultiValueDictKeyError
 import user.auth as auth
 
 # Create your views here.
-@csrf_exempt
 def register(request):
     response = HttpResponse()
     if request.method == "POST":

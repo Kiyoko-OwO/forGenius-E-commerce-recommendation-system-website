b74c731ed5030eda9e8a1e295d9c8b6f3335d14f
Tianchen Yang <z5248280@ad.unsw.edu.au>
2021-10-18 16:13:51 +0800

    add: add product system

--- forGenius/product/products.py
+++ forGenius/product/products.py
@@ -1,6 +1,12 @@
 from product.models import Product, Features
 from product.errors import ProductIdError
+from user.models import Admin
 
+ADMIN_EMAIL = '3900forgenius@gmail.com'
+
+
+def get_Admin():
+    return Admin.objects.get()
 
 def product_userView(product_id):
     try:
@@ -17,6 +23,46 @@ def product_userView(product_id):
         } 
     return info
 
+def admin_add_product(name, description, warranty, delivery_date, sales_data, price):    
+    reg = Product(admin_email=get_Admin(), name=name, description=description, warranty=warranty, delivery_date=delivery_date, sales_data=sales_data, price=price)
+    reg.save()
 
+def admin_edit_product(product_id, name, description, warranty, delivery_date, sales_data, price):    
+    try:
+        reg = Product.objects.get(product_id=product_id)
+    except Product.DoesNotExist:
+        raise ProductIdError("This product does not exist")
+    reg.name = name
+    reg.description = description
+    reg.warranty = warranty
+    reg.delivery_date = delivery_date
+    reg.sales_data = sales_data
+    reg.price = price    
+    reg.save()
 
+def admin_delete_product(product_id):    
+    try:
+        reg = Product.objects.get(product_id=product_id)
+    except Product.DoesNotExist:
+        raise ProductIdError("This product does not exist")
+    reg.delete()
     
+def admin_products_all():
+    try:        
+        books = Product.objects.all()
+    except:
+        pass
+    info_str = "product_details"
+    data = { info_str : []}
+    for book in books:
+        info = {
+            "product_id" : book.product_id,
+            "name" : book.name,
+            "warranty" : book.warranty,
+            "description" : book.description,
+            "delivery_date" : book.delivery_date,
+            "sales_data" : book.sales_data,
+            "price" : book.price,
+        } 
+        data[info_str].append(info)
+    return data
--- forGenius/product/views.py
+++ forGenius/product/views.py
@@ -3,10 +3,19 @@ from django.http import HttpResponse
 from django.utils.datastructures import MultiValueDictKeyError
 from django.http.response import JsonResponse
 from product.errors import ProductIdError
+from user.errors import InputError
 import product.products as products
+import user.auth as auth
 import json
 
 # Create your views here.
+def isFloat(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
 
 def view_product_user(request):
  
@@ -33,6 +42,159 @@ def view_product_user(request):
 
         try:
             data = products.product_userView(product_id)
+        except ProductIdError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        response.status_code = 200
+        meta = {'msg' : 'OK', 'status': 200}
+        jsons = {
+            'data' : data,
+            'meta' : meta
+        }
+        return JsonResponse(jsons)
+    response.status_code = 405
+    return response
+
+def admin_add_product(request):
+    response = HttpResponse()
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            response.status_code = 441
+            response.content = 'json.JSONDecodeError'
+            return response
+        try:
+            #token = data["token"]
+            #email = auth.token_to_email(token)
+            name = data['name']
+            description = data['description']
+            warranty = data['warranty']
+            delivery_date = data['delivery_date']
+            sales_data = data['sales_data']
+            price = data['price']
+            if not isFloat(price):
+                response.status_code = 443
+                response.content = 'price is not float'
+                return response
+        except KeyError:
+            response.status_code = 442
+            response.content = 'KeyError'
+            return response
+        except InputError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        try:
+            data = products.admin_add_product(name, description, warranty, delivery_date, sales_data, price)
+        except ProductIdError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        response.status_code = 200
+        return response
+    response.status_code = 405
+    return response
+
+def admin_edit_product(request):
+    response = HttpResponse()
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            response.status_code = 441
+            response.content = 'json.JSONDecodeError'
+            return response
+        try:
+            #token = data["token"]
+            #email = auth.token_to_email(token)
+            name = data['name']
+            description = data['description']
+            warranty = data['warranty']
+            delivery_date = data['delivery_date']
+            sales_data = data['sales_data']
+            price = data['price']
+            product_id = data['product_id']
+            if not isFloat(price):
+                response.status_code = 443
+                response.content = 'price is not float'
+                return response
+        except KeyError:
+            response.status_code = 442
+            response.content = 'KeyError'
+            return response
+        except InputError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        try:
+            data = products.admin_edit_product(product_id, name, description, warranty, delivery_date, sales_data, price)
+        except ProductIdError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        response.status_code = 200
+        return response
+    response.status_code = 405
+    return response
+        
+def admin_delete_product(request):
+    response = HttpResponse()
+    if request.method == "DELETE":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            response.status_code = 441
+            response.content = 'json.JSONDecodeError'
+            return response
+        try:
+            #token = data["token"]
+            #email = auth.token_to_email(token)
+            product_id = data['product_id']
+        except KeyError:
+            response.status_code = 442
+            response.content = 'KeyError'
+            return response
+        except InputError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        try:
+            products.admin_delete_product(product_id)
+        except ProductIdError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        response.status_code = 200
+        return response
+    response.status_code = 405
+    return response
+        
+
+def admin_products_all(request):
+    response = HttpResponse()
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            response.status_code = 441
+            response.content = 'json.JSONDecodeError'
+            return response
+        try:
+            pass
+            #token = data["token"]
+            #email = auth.token_to_email(token)
+        except KeyError:
+            response.status_code = 442
+            response.content = 'KeyError'
+            return response
+        except InputError as e:
+            response.status_code = 400
+            response.content = e
+            return response
+        try:
+            data = products.admin_products_all()
         except ProductIdError as e:
             response.status_code = 400
             response.content = e

7ef353a2f6f1aacbd7bddfd9233fae651ed6c358
Tianchen Yang <z5248280@ad.unsw.edu.au>
2021-11-10 15:55:44 +0800

    update: update the recommendation

--- forGenius/product/recommendation.py
+++ forGenius/product/recommendation.py
@@ -4,17 +4,42 @@ from user.models import User, Interest
 from user.errors import InputError
 import random
 
+recommendationDB = {}
 # return the recommendation if the user is logout(guest mode)
 def public_recommendation():
+    products = recomment_by_sales()
+    # output the products with ordered number
+    products = pick_products(products, 6)
+    data = {
+        "product_number": len(products),
+        "products": products,
+    }
+    return data
 
+# return the recommendation if the user is logged (user mode)
+def private_recommendation(email):
+    # search the user email to find its interest
+    try:
+        user_email = User.objects.get(pk=email)
+    except User.DoesNotExist:
+        raise InputError('User not exist')
+        
+
+    return pick_products(data, 6)
+
+
+# pick selected number of the products from the recommendation lists
+def pick_products(product_list, num):
+    if len(product_list) <= num:
+        return product_list
+    # use random algorithms to select
+    samples = random.sample(product_list, num)
+    return product_list
+
+def recomment_by_sales():    
     # sort the product with selling data
     top_selling_products = Product.objects.order_by('-sales_data')
-    data = {
-        "product_number": 0,
-        "products": [],
-    }
-    product_number = 0
-
+    data = []
     # output the relevant selling products
     for product in top_selling_products:
         info = {
@@ -26,22 +51,11 @@ def public_recommendation():
             "features": get_product_features(product.product_id),
             "sales_data": product.sales_data
         }
-        data["products"].append(info)
-        product_number += 1
+        data.append(info)
+    return data
 
-    data["product_number"] = product_number
-    # output the products with ordered number
-    return pick_products(data, 6)
-
-# return the recommendation if the user is logged (user mode)
-def private_recommendation(email):
-    # search the user email to find its interest
-    try:
-        user_email = User.objects.get(pk=email)
-    except User.DoesNotExist:
-        raise InputError('User not exist')
-    data = public_recommendation()
-
+def recomment_by_interest(user_email):
+    data = []
     # put relevent products with interests in the recommendation array
     interests = Interest.objects.filter(user_email=user_email)
     for interest in interests:
@@ -56,23 +70,28 @@ def private_recommendation(email):
                 "features": get_product_features(product.product_id),
                 "sales_data": product.sales_data
             }
-            if info not in data["products"]:
-                data["products"].append(info)
-                data["product_number"] += 1
+            if info not in data:
+                data.append(info)
     # sort the products with selling datas
-    data["products"] = sorted(data["products"], key=lambda x: -x["sales_data"])
-    return pick_products(data, 6)
+    data = sorted(data, key=lambda x: -x["sales_data"])
+    return data
 
-# pick selected number of the products from the recommendation lists
-def pick_products(product_list, num):
-    if product_list["product_number"] <= num:
-        return product_list
-    # use random algorithms to select
-    samples = random.sample(product_list["products"], num)
-    data = {
-        "product_number": num,
-        "products": samples,
-    }
+def recomment_by_search(user_email):
+    data = []
+    if user_email in recommendationDB:
+        for product_id in recommendationDB[user_email]:
+            product =  Product.objects.get(product_id = product_id)
+            info = {
+                "product_id": product.product_id,
+                "name": product.name,
+                "description": product.description,
+                "price": round(float(product.price), 2),
+                "picture": product.picture,
+                "features": get_product_features(product.product_id),
+                "sales_data": product.sales_data
+            }
+            if info not in data:
+                data.append(info)
     return data
 
 # balance recommendation lists to selected number of the products
